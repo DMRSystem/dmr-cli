10490672ff89caa2088a5114767d002a34de0615
Connor Boyle <boylecj@rose-hulman.edu>
2018-05-29 23:30:44 -0400

    Composite pattern for ISetupStep

--- src/main/python/dmr/controller/setup/behavior/osx/step/mixxx_installation/mixxx_app_copier.py
+++ src/main/python/dmr/controller/setup/behavior/osx/step/mixxx_installation/mixxx_app_copier.py
@@ -20,19 +20,3 @@ class MixxxInstaller(ISetupStep):
         self.logger.info('Mixxx.app copied.', indentation=1)
         self.logger.info('Mixxx installed.', indentation=1)
 
-
-class AlreadyInstalledCheckDecorator(AbstractSetupStepDecorator):
-
-    def __init__(self, setup_step: ISetupStep, logging_adapter: ILoggingAdapter):
-        AbstractSetupStepDecorator.__init__(self, setup_step)
-        self.logger = logging_adapter
-
-    def execute(self):
-        if self._mixxx_already_installed():
-            self.logger.info('Mixxx already installed at /Applications/Mixxx.app', 'blue')
-            self.logger.info('Skipping installation.', indentation=1)
-            return
-        AbstractSetupStepDecorator.execute(self)
-
-    def _mixxx_already_installed(self):
-        return Path('/', 'Applications', 'Mixxx.app').exists()
--- src/main/python/dmr/controller/setup/behavior/osx/step/mixxx_installation/mixxx_downloader.py
+++ src/main/python/dmr/controller/setup/behavior/osx/step/mixxx_installation/mixxx_downloader.py
@@ -4,6 +4,7 @@ from dmr.utils.logging.logging_adapter import ILoggingAdapter
 from dmr.controller.setup.setup_step import ISetupStep, AbstractSetupStepDecorator
 
 from pathlib import Path
+import sys
 
 
 class MixxxDownloader(ISetupStep):
@@ -39,4 +40,21 @@ class AlreadyDownloadedCheckDecorator(AbstractSetupStepDecorator):
             self.logger.info('Skipping download.', indentation=1)
             return
         AbstractSetupStepDecorator.execute(self)
+
+
+class AlreadyInstalledCheckDecorator(AbstractSetupStepDecorator):
+
+    def __init__(self, setup_step: ISetupStep, logging_adapter: ILoggingAdapter):
+        AbstractSetupStepDecorator.__init__(self, setup_step)
+        self.logger = logging_adapter
+
+    def execute(self):
+        if self._mixxx_already_installed():
+            self.logger.info('Mixxx already installed at /Applications/Mixxx.app', 'blue')
+            self.logger.info('Skipping installation.', indentation=1)
+            sys.exit(0)
+        AbstractSetupStepDecorator.execute(self)
+
+    def _mixxx_already_installed(self):
+        return Path('/', 'Applications', 'Mixxx.app').exists()
 
